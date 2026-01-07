8fa38a6f0822e614b1a72d5d9914fe11374c05da
gitsayed <abucollge@gmail.com>
2026-01-07 10:25:49 +0600

    EDIT

--- frontend/src/app/_helpers/auth.interceptor.ts
+++ frontend/src/app/_helpers/auth.interceptor.ts
@@ -1,12 +1,16 @@
-import { HTTP_INTERCEPTORS,HttpHandler,
+import {
+  HTTP_INTERCEPTORS, HttpHandler,
   HttpInterceptor,
-  HttpRequest, HttpEvent } from '@angular/common/http';
+  HttpRequest, HttpEvent,
+  HttpErrorResponse
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { TokenStorageService } from '../_services/token-storage.service';
-import { Observable } from 'rxjs';
-import { finalize } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, finalize } from 'rxjs/operators';
 import { LoaderService } from '../_loader/loader.service';
+import { ToasterService } from '../_services/toaster.service';
 
 
 
@@ -14,6 +18,7 @@ import { LoaderService } from '../_loader/loader.service';
 export class AuthInterceptor implements HttpInterceptor {
   constructor(
     private tokenService: TokenStorageService,
+    private toast: ToasterService,
     private loaderService: LoaderService) { }
 
   private activeRequests = 0;
@@ -37,12 +42,27 @@ export class AuthInterceptor implements HttpInterceptor {
       : req;
 
     return next.handle(authReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+
+        if (error.status === 401) {
+          this.toast.warning("session timeout.")
+          this.logout();
+        }
+
+        return throwError(() => error);
+      }),
       finalize(() => {
         this.activeRequests--;
         if (this.activeRequests === 0) {
         }
       })
     );
+  }
+
+  logout() {
+    sessionStorage.clear();
+    window.sessionStorage.clear();
+    window.location.reload();
   }
 }
 
